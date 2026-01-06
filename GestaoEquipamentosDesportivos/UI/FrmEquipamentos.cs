25e6c3e4128adc4f7a4f86df589765b878058015
francisco.3.fernandes@gmail.com <francisco.3.fernandes@gmail.com>
2026-01-06 14:55:37 +0000

    Inicio do Projeto

---
+++ GestaoEquipamentosDesportivos/UI/FrmEquipamentos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestaoEquipamentosDesportivos.UI
+{
+    public partial class FrmEquipamentos : Form
+    {
+        public FrmEquipamentos()
+        {
+            InitializeComponent();
+        }
+    }
+}
