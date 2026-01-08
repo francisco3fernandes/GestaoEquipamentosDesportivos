7c9efbadcfdbf744e4749990fe81c7c755828ab6
francisco.3.fernandes@gmail.com <francisco.3.fernandes@gmail.com>
2026-01-08 19:06:35 +0000

    Consulta de Equipamentos por Modalidade

--- GestaoEquipamentosDesportivos/UI/FrmConsulta.Designer.cs
+++ GestaoEquipamentosDesportivos/UI/FrmConsulta.Designer.cs
@@ -28,12 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmConsulta";
+            cmbModalidades = new ComboBox();
+            btnConsultar = new Button();
+            dgvEquipamentos = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dgvEquipamentos).BeginInit();
+            SuspendLayout();
+            // 
+            // cmbModalidades
+            // 
+            cmbModalidades.Font = new Font("Times New Roman", 10.2F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            cmbModalidades.FormattingEnabled = true;
+            cmbModalidades.Location = new Point(12, 12);
+            cmbModalidades.Name = "cmbModalidades";
+            cmbModalidades.Size = new Size(151, 27);
+            cmbModalidades.TabIndex = 0;
+            // 
+            // btnConsultar
+            // 
+            btnConsultar.Font = new Font("Times New Roman", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnConsultar.Location = new Point(339, 412);
+            btnConsultar.Name = "btnConsultar";
+            btnConsultar.Size = new Size(94, 29);
+            btnConsultar.TabIndex = 1;
+            btnConsultar.Text = "Consultar";
+            btnConsultar.UseVisualStyleBackColor = true;
+            btnConsultar.Click += btnConsultar_Click;
+            // 
+            // dgvEquipamentos
+            // 
+            dgvEquipamentos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvEquipamentos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvEquipamentos.Location = new Point(43, 94);
+            dgvEquipamentos.Name = "dgvEquipamentos";
+            dgvEquipamentos.ReadOnly = true;
+            dgvEquipamentos.RowHeadersWidth = 51;
+            dgvEquipamentos.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvEquipamentos.Size = new Size(694, 253);
+            dgvEquipamentos.TabIndex = 2;
+            // 
+            // FrmConsulta
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(782, 453);
+            Controls.Add(dgvEquipamentos);
+            Controls.Add(btnConsultar);
+            Controls.Add(cmbModalidades);
+            Name = "FrmConsulta";
+            SizeGripStyle = SizeGripStyle.Hide;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Consulta de Equipamentos por Modalidade";
+            Load += FrmConsulta_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvEquipamentos).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ComboBox cmbModalidades;
+        private Button btnConsultar;
+        private DataGridView dgvEquipamentos;
     }
 }
--- GestaoEquipamentosDesportivos/UI/FrmConsulta.cs
+++ GestaoEquipamentosDesportivos/UI/FrmConsulta.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GestaoEquipamentosDesportivos.DataAccess;
+using GestaoEquipamentosDesportivos.Models;
 
 namespace GestaoEquipamentosDesportivos.UI
 {
     public partial class FrmConsulta : Form
     {
+        private readonly SqlDataAccess _dataAccess = new SqlDataAccess();
+
         public FrmConsulta()
         {
             InitializeComponent();
+            CarregarModalidades();
+        }
+
+        private void CarregarModalidades()
+        {
+            cmbModalidades.DataSource = _dataAccess.ListarModalidades();
+            cmbModalidades.DisplayMember = "Nome";
+            cmbModalidades.ValueMember = "IdModalidade";
+        }
+
+        private void FrmConsulta_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            int idModalidade = (int)cmbModalidades.SelectedValue;
+            dgvEquipamentos.DataSource = _dataAccess.ListarEquipamentosPorModalidade(idModalidade);
         }
     }
 }
